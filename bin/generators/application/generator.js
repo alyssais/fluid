2996918ac5eb235b12f7a0a2c5d9cd54b81ee174
Alyssa Ross <hi@alyssa.is>
2014-01-03 16:25:40 +0000

    Generators.

---
+++ bin/generators/application/generator.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+var async = require("async");
+
+module.exports = function(name) {
+	fs.mkdir(name, function(error) {
+		fs.mkdir(name + "/config", function(error) {
+			var copyTemplate = function(location) {
+				var read = fs.createReadStream(__dirname + '/templates/' + location);
+				var write = fs.createWriteStream(name + "/" + location);
+				read.pipe(write);
+			};
+
+			['config/application.js', 'config/routes.js'].forEach(copyTemplate);
+		});
+
+		fs.mkdir(name + "/app", function(error) {
+			fs.mkdir(name + "/app/controllers");
+			fs.mkdir(name + "/app/views");
+		});
+	});
+}

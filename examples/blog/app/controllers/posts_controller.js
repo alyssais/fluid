c711b8901387db64ff59ccd8858842d1d228d3c8
Alyssa Ross <hi@alyssa.is>
2014-01-29 23:21:16 +0000

    Refactoring to avoid overwriting #call.

--- examples/blog/app/controllers/posts_controller.js
+++ examples/blog/app/controllers/posts_controller.js
@@ -1,5 +1,16 @@
 module.exports = new Flume.Controller(function() {
+  before(function() {
+    console.log("before all action was called");
+  });
+
+  before('index', function() {
+    console.log("before index action was called.")
+  });
+
 	this.index = function() {
+    console.log("index action was called.");
+
+    // data assigned to `this` will be passed to the view.
 		this.posts = [{
 			title: "Hello world",
 			content: "Lorem ipsum dolor sit amet."
@@ -8,4 +19,13 @@ module.exports = new Flume.Controller(function() {
 			content: "This is fun."
 		}];
 	};
+
+  after(function() {
+    console.log("after all action was called.");
+  });
+
+  after('index', function() {
+    console.log("after index action was called.");
+  });
 });
+
